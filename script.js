8633c8e3bf055c8ddd05ddf88b32daff7878fe8d
Ume-Aham Sylvester <117973007+gittybro12@users.noreply.github.com>
2024-07-08 14:23:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const info = document.querySelector('.information')
+    const low = document.querySelector('.btn')
+    const close = document.querySelector('.close')
+    
+    low.style.display = "none"
+    low.addEventListener("click",display)
+    
+    function display(e){
+        info.style.display ="block"
+    }
+    
+    close.addEventListener('click',()=>{
+        info.style.display ="none"
+        low.innerHTML = "click to display again"
+        low.style.display = "block"
+        
+    })
+    window.addEventListener("load",function(){
+        this.setTimeout(function(){
+            info.style.display ="block"
+        },5000)
+    })
+})
